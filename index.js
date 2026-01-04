4804ea1fd8830729105bd363e3ebd3e012583a8f
Yossi Arye <aryeyossi@gmail.com>
2026-01-04 19:21:33 +0200

    res.json({websites})

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/websites', async function (req, res) {
     const websites = await Website.find({})
     console.log('websites')
     console.log(websites)
-    res.json({websites, build: Date.now()})
+    res.json({websites})
   } catch (error) {
     return res.status(500).send(error.message)
   }
