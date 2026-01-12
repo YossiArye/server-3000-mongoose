fde4324c883b5cd979c53f6454a7d48c531b946f
unknown <aryeyossi@gmail.com>
2026-01-12 21:00:36 +0200

    test

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const PORT = process.env.PORT || 3000
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/entre_db'
 
 mongoose.connect(MONGO_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err))
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err))
 
 app.get('/websites', async function (req, res) {
   try {
     const websites = await Website.find({})
     console.log('websites')
     console.log(websites)
-    res.json({websites})
+    res.json({ websites })
   } catch (error) {
     return res.status(500).send(error.message)
   }
@@ -55,10 +55,10 @@ app.get('/role/:id', async function (req, res) {
 
 app.get('/', async function (req, res) {
   try {
-    console.log('arrived')
+    console.log('arriveddddddddd')
     const users = await User.find({})
     // Removed setTimeout to rely on DB speed, but can re-add if latency simulation is needed
-    res.json(users)
+    res.json({ users, message: 'users' })
   } catch (error) {
     return res.status(500).send(error.message)
   }
@@ -79,12 +79,12 @@ app.post('/', async function (req, res) {
 
     for (const userData of inputs) {
       if (!userData.id) {
-          currentId++
-          userData.id = currentId
+        currentId++
+        userData.id = currentId
       }
       newUsers.push(userData)
     }
-    
+
     const createdUsers = await User.insertMany(newUsers)
     console.log(createdUsers)
     res.json(createdUsers)
